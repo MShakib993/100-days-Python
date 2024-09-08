ded6841e71cd10dcd6bf2daf18a226930a0b25c2
MShakib993 <149675660+MShakib993@users.noreply.github.com>
2024-09-08 23:49:28 +0530

    Day 7

---
+++ Day 7/Day_7.py
@@ -0,0 +1,8 @@
+matrix = []
+for row in range(2):
+    row1 = []
+    for col in range(3):
+        element = int(input("write a num: "))
+        row1.append(element)
+    matrix.append(row1)
+print(matrix)
